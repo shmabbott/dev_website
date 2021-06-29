e42f84c93f8c9484052e48c31fa24de288ae406b
Sam Abbott <samgeoabbott@gmail.com>
2021-06-29 15:58:07 -0700

    Initial footer files needed for dev work.

---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import * as navbarStyles from "./footer.module.css"
+
+
+const Footer = ({ location, title, children }) => {
+    return(<div></div>);
+}
+
+export default Footer;

7da569f2c04d6aacafde304c090ee089983c4454
Sam Abbott <samgeoabbott@gmail.com>
2021-07-11 10:44:14 -0700

    Updated layout to support different CSS styles based on the page being viewed.

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,19 +7,37 @@ const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
 
-  return (
-    <div className="main-layout-container" data-is-root-path={isRootPath} >
-      <header>
-        <Navbar location={location} title={title}></Navbar>
-      </header>
-      <div className="global-wrapper" >
+  console.log('Sam Search Here '+isRootPath);
+
+  if(isRootPath){
+    console.log('Root path if statement is true');
+    return(
+      <div className="main-layout-container" data-is-root-path={isRootPath} >
+        <header>
+          <Navbar location={location} title={title}></Navbar>
+        </header>
         <main>{children}</main>
+        <footer id="footer1">
+          <Footer></Footer>
+        </footer>
       </div>
-      <footer id="footer1">
-        <Footer></Footer>
-      </footer>
-    </div>
-  )
+    )
+  }else{
+    return (
+      <div className="main-layout-container" data-is-root-path={isRootPath} >
+        <header>
+          <Navbar location={location} title={title}></Navbar>
+        </header>
+        <div className="global-wrapper" >
+          <main>{children}</main>
+        </div>
+        <footer id="footer1">
+          <Footer></Footer>
+        </footer>
+      </div>
+    )
+  }
+
 }
 
 export default Layout
